9f910e370fcf8dc7ce2e72040746f858e0bca9df
amschel99 <6290.2020@students.ku.ac.ke>
2022-12-14 11:50:25 +0300

    paperprops

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import Drawer from '../components/Drawer'
 import AddNewCar from '../components/Dashboard/AdminParts/AddNewCar'
-import {Box} from "@mui/material"
+
+import {Stack} from "@mui/material"
 
 const Profile = () => {
   return (
-   <Box sx={{width:'100vw',display:'flex'}}>
-<Box sx={{flex:1}}>
+  <Stack direction="row" spacing={2} p={2}>
 <Drawer/>
-</Box>
-<Box sx={{flex:1}}>
+
 <AddNewCar/>
-</Box>
+
+</Stack>
     
-   </Box>
+  
   )
 }
 
