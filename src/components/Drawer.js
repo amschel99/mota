73278ede6576a6d9398c21b1cd429c9f3b337941
amschel99 <6290.2020@students.ku.ac.ke>
2022-12-14 10:21:22 +0300

    changed the Drawer's z index

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -31,7 +31,7 @@ const DrawerComponent = () => {
         </>
     }
   return (
-    <Drawer variant='permanent' sx={{'z-index':5, display:{xs:'none',sm:'block'}}} >
+    <Drawer variant='permanent' sx={{'z-index':0, display:{xs:'none',sm:'block'}}} >
 <List sx={{marginTop:7}}>
 <ListSubheader>Actions</ListSubheader>
 <ListItems items={items.Actions}/>
