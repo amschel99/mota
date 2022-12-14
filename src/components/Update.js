bff8b13bd0cec96564e35a828e6ac3ec91620480
amschel99 <6290.2020@students.ku.ac.ke>
2022-12-14 12:20:01 +0300

    paperprops

--- src/components/Update.js
+++ src/components/Update.js
@@ -13,7 +13,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Alert from '@mui/material/Alert';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import {useAuth} from "../others/useAuthContext"
+import useAuth from "../others/useAuthContext"
 import {useHistory} from 'react-router-dom'
 import axios from 'axios'
 
