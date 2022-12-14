a36954462d275346bf407f4b7303821253318c88
amschel99 <6290.2020@students.ku.ac.ke>
2022-12-14 11:17:48 +0300

    paperprops

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -31,7 +31,15 @@ const DrawerComponent = () => {
         </>
     }
   return (
-    <Drawer variant='permanent' sx={{'z-index':5, backgroundColor:'transparent', display:{xs:'none',sm:'block'}}} >
+    <Drawer
+      PaperProps={{
+    sx: {
+      width: 240,
+       backgroundColor:'transparent',
+       'z-index':5
+    }
+  }}
+    variant='permanent' sx={{'z-index':5, backgroundColor:'transparent', display:{xs:'none',sm:'block'}}} >
 <List sx={{marginTop:25}}>
 <ListSubheader>Actions</ListSubheader>
 <ListItems items={items.Actions}/>
