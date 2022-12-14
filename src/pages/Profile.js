9f78c093fe4798148ae252057f8d385531c5d22e
amschel99 <6290.2020@students.ku.ac.ke>
2022-12-14 19:57:26 +0300

    bug fixing

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -20,7 +20,7 @@ const Profile = () => {
     <IconButton
                onClick={()=>setOpen((prev)=>!prev)}
                 aria-label="Menu"
-                sx={{display:{xs:"block",sm:"none"},color:'red',position:'absolute',top:'25vw'}}
+                sx={{display:{xs:"block",sm:"none"},color:'red',position:'absolute',top:'15vw'}}
                 >
                     <MenuIcon/>
                 </IconButton>
