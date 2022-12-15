da16a125e747066f43260e07c85b9f19f047f6e4
amschel99 <6290.2020@students.ku.ac.ke>
2022-12-15 10:42:37 +0300

    separation of concerns

--- src/components/CarsSection/CarsSection.js
+++ src/components/CarsSection/CarsSection.js
@@ -1,9 +1,10 @@
-import { Grid, Typography,Slider } from '@mui/material';
+import { Grid, Typography} from '@mui/material';
 import { Box } from '@mui/system';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import LoadingSpinner from '../Common/LoadingSpinner/LoadingSpinner';
 import SingleCar from './SingleCar/SingleCar';
+import PriceSlider from '../CarsSection/../../components/Slider'
 import {   useSelector} from 'react-redux'
 
 
@@ -14,32 +15,8 @@ const {type}= useSelector((state)=>state.type)
 
     const [cars, setCars] = useState(null);
 
-const[price,setPrice]=React.useState(20000000)
-    const marks = [
-   
-  {
-    value: 1000000,
-    label: '1m',
-  },
- 
-  
-  {
-    value:5000000,
-    label: '5m',
-  },
-     {
-    value:10000000,
-    label: '10m',
-  },
-   {
-    value:15000000,
-    label: '15m',
-  },
-   {
-    value:20000000,
-    label: '20m',
-  },
-];
+
+    const[price,setPrice]=React.useState(20000000)
 
     useEffect(() => {
          const FetchCars = async () => {
@@ -62,26 +39,10 @@ const[price,setPrice]=React.useState(20000000)
         FetchCars()   
     }, [dataAmount,search,type,price])
 
-const getText=(value)=>`${value}`
+
     return (!cars ? <LoadingSpinner style={{ padding: '100px 0' }} /> :
         <Box sx={{ maxWidth: 'xl', mx: 'auto', p: 2, justifyContent: "center" }}>
-            <Typography variant="h4" align="center" color="primary" fontWeight="bold" mb={6}>Filter by price</Typography>
-             <Box sx={{ width: {md:'80vw'} }}>
-      <Slider
-        aria-label="Price Range"
-        
-       min={500000}
-       max={20000000}
-       step={null}
-        getAriaValueText={getText}
-        valueLabelDisplay="auto"
-        marks={marks}
-        value={price}
-      
-        onChange={(e,value)=>{setPrice(value)}}
-       
-      />
-    </Box>
+           <PriceSlider price={price} setPrice={setPrice}/>
             <Typography variant="h4" align="center" color="primary" fontWeight="bold" mb={6}>Popular Cars</Typography>
             <Grid container rowSpacing={4} columnSpacing={{ xs: 1, sm: 2, md: 3, xl: 4 }}
                 sx={{ justifyContent: 'center' }}>
---
+++ src/components/Slider.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import {Box,Typography,Slider} from "@mui/material"
+
+const PriceSlider = ({price,setPrice}) => {
+
+    const marks = [
+   
+  {
+    value: 1000000,
+    label: '1m',
+  },
+ 
+  
+  {
+    value:5000000,
+    label: '5m',
+  },
+     {
+    value:10000000,
+    label: '10m',
+  },
+   {
+    value:15000000,
+    label: '15m',
+  },
+   {
+    value:20000000,
+    label: '20m',
+  },
+];
+const getText=(value)=>`${value}`
+  return (
+    <Box>
+     <Typography variant="h4" align="center" color="primary" fontWeight="bold" mb={6}>Filter by price</Typography>
+             <Box sx={{ width: {md:'80vw'} }}>
+      <Slider
+        aria-label="Price Range"
+        
+       min={500000}
+       max={20000000}
+       step={null}
+        getAriaValueText={getText}
+        valueLabelDisplay="auto"
+        marks={marks}
+        value={price}
+      
+        onChange={(e,value)=>{setPrice(value)}}
+       
+      />
+    </Box>
+    </Box>
+  )
+}
+
+export default PriceSlider
