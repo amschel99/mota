391274af6a3e17fb2d9ea517ee628d93a1b8643e
amschel99 <6290.2020@students.ku.ac.ke>
2022-12-15 15:56:01 +0300

    still fixing bugs

--- src/components/Common/Navbar2/Navbar2.js
+++ src/components/Common/Navbar2/Navbar2.js
@@ -1,71 +1,51 @@
-import React from "react";
-import "./Navbar2.css";
-import Box from "@mui/material/Box";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-
-import MenuIcon from "@mui/icons-material/Menu";
-
-import { NavLink } from "react-router-dom";
+import React,{useState,useEffect} from "react"
+import {AppBar,Toolbar,IconButton,Slide,Box} from "@mui/material"
+import {NavLink} from "react-router-dom"
+import MenuIcon from "@mui/icons-material/Menu"
 
 
 
 
 
+const Navbar2= ({setOpen})=>{
+    const [scrolling,setScrolling]=useState(false)
+    const [scrollTop, setScrollTop]=useState(0)
 
 
-function changeHeaderOnScroll() {
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 150)
-      document.querySelector("header").style.padding = "10px 0";
-    else if (window.scrollY < 100)
-      document.querySelector("header").style.padding = "20px 0";
-  });
-}
 
-const Navbar2 = ({setOpen}) => {
-const [anchorEl, setAnchorEl] = React.useState(null);
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+    useEffect(()=>{
+           function handleScroll(e){
+        setScrollTop(e.target.documentElement.scrollTop)
+        setScrolling(e.target.documentElement.scrollTop>scrollTop)
+    }
 
- 
-  
 
-  return (
-    <>
-      <AppBar
-        position="sticky"
-        sx={{
-          background: "white",
-          boxShadow: "initial",
-          maxHeight: "200px",
-          overflow: "hidden",
-          transition: "500ms ease",
-        }}
-        onLoad={changeHeaderOnScroll}
-      >
-        <Toolbar
-          sx={{
-            width: "100vw",
-            maxWidth: "100vw",
-            margin: "auto",
-          }}
-        >
-          <Box
-            sx={{
-              width: "100%",
-              display: "flex",
-              justifyContent: { xs: "space-around", sm: "space-between" },
-              marginLeft: { xs: "0", sm: "0" },
-            }}
-          >
-            <Box sx={{ display: { xs: "block", sm: "block" } }} flex={2}>
-              <Box
+        window.addEventListener('scroll',handleScroll)
+        return ()=>{
+            window.removeEventListener('scroll', handleScroll)
+        }
+
+    },[scrolling,scrollTop])
+
+    useEffect(()=>{
+       return  setScrolling((prev)=>prev!==scrolling)
+    },[scrolling])
+
+
+
+    return (
+        <div   >
+            <Slide in={!scrolling}>
+         <AppBar position="fixed">
+            <Toolbar>
+                <IconButton
+               onClick={()=>setOpen((prev)=>!prev)}
+                aria-label="Menu"
+                sx={{display:{xs:"block",sm:"none"},color:'red',position:'absolute',top:'0vw',marginBottom:'5px',left:'0vw'}}
+                >
+                    <MenuIcon/>
+                </IconButton>
+               <Box
                 sx={{ width: { xs: "25vw", sm: "20vw" } }}
                 component={NavLink}
                 to="/"
@@ -78,28 +58,16 @@ const [anchorEl, setAnchorEl] = React.useState(null);
                 />
 
               </Box>
-            </Box>
+               
+            </Toolbar>
 
-            {/*logo goes up */}
-           
-            <Box flex={2}>
-              <Box id="header-menu-toggler">
-               <IconButton
-               onClick={()=>setOpen((prev)=>!prev)}
-                aria-label="Menu"
-                sx={{display:{xs:"block",sm:"none"},color:'red',position:'absolute',top:'0vw',marginBottom:'5px',left:'0vw'}}
-                >
-                    <MenuIcon/>
-                </IconButton>
-              </Box>
- 
-             
-            </Box>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </>
-  );
-};
 
-export default Navbar2;
+         </AppBar>
+          </Slide>
+
+
+
+        </div>
+    )
+}
+export default Navbar2
