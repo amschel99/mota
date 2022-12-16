3ade0b89ff94d3cb0a02efd5074f400535fa1f77
amschel99 <6290.2020@students.ku.ac.ke>
2022-12-16 16:39:34 +0300

    changed the width of the social flow buttons

--- src/components/CarsSection/CarsSection.js
+++ src/components/CarsSection/CarsSection.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import LoadingSpinner from '../Common/LoadingSpinner/LoadingSpinner';
 import SingleCar from './SingleCar/SingleCar';
-//import PriceSlider from '../CarsSection/../../components/Slider'
+import PriceSlider from '../CarsSection/../../components/Slider'
 import {   useSelector} from 'react-redux'
 
 
@@ -45,7 +45,7 @@ const {type}= useSelector((state)=>state.type)
     return (!cars ? <LoadingSpinner style={{ padding: '100px 0' }} /> :
         <Box sx={{ maxWidth: 'xl', mx: 'auto', p: 2, justifyContent: "center" }}>
 
-          {/** <PriceSlider price={price} setPrice={setPrice}/> */}
+      <PriceSlider price={price} setPrice={setPrice}/> 
 
             <Typography variant="h4" align="center" color="primary" fontWeight="bold" mb={6}>Popular Cars</Typography>
             <Grid container rowSpacing={4} columnSpacing={{ xs: 1, sm: 2, md: 3, xl: 4 }}
