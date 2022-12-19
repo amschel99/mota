a04878d7d999f985d913f7bfcf17de45d2a6be07
amschel99 <6290.2020@students.ku.ac.ke>
2022-12-19 20:25:32 +0300

    solved a bug

--- src/pages/Seller.js
+++ src/pages/Seller.js
@@ -12,6 +12,7 @@ import {useParams} from "react-router-dom";
 import SocialFlow from "../components/Common/socialFlow/socialFlow";
 
 const Seller = ({ dataAmount }) => {
+    const {cars}= useSelector((state)=>state.cars)
       const { userId } = useParams();
       const[userEmail,setUserEmail]=React.useState('')
       const [user,setUser]=React.useState('')
@@ -22,10 +23,10 @@ const Seller = ({ dataAmount }) => {
       const[ twitter,setTwitter]=React.useState('')
       const[instagram,setInstagram]=React.useState('')
       
-const {search}= useSelector((state)=>state.query)
-const {type}= useSelector((state)=>state.type)
+//const {search}= useSelector((state)=>state.query)
+//const {type}= useSelector((state)=>state.type)
 
-    const [cars, setCars] = useState(null);
+   // const [cars, setCars] = useState(null);
 useEffect(()=>{
 const getUser= async ()=>{
     try{
@@ -46,23 +47,25 @@ return null;
 getUser()
 },[userId])
 
-    useEffect(() => {
-         const FetchCars = async () => {
+
+  const carDetails= cars.find((car)=>car.user===userEmail)
+    //useEffect(() => {
+        // const FetchCars = async () => {
             
-        const {data} =  await axios.get(`https://milesmotors.onrender.com/cars/all?user=${userEmail}`,{
-            params:{
-                name:search,
-                brand:type
-            }
-        })
-        setCars(data)
+      //  const {data} =  await axios.get(`https://milesmotors.onrender.com/cars/all?user=${userEmail}`,{
+       //     params:{
+         //       name:search,
+       //         brand:type
+        //    }
+       // })
+       // setCars(data)
        
-    }
-        FetchCars()   
-    }, [dataAmount,search,type,userEmail])
+   // }
+      //  FetchCars()   
+  //  }, [dataAmount,search,type,userEmail])
 
 
-    return (!cars ? <LoadingSpinner style={{ padding: '100px 0' }} /> :
+    return (!carDetails ? <LoadingSpinner style={{ padding: '100px 0' }} /> :
         <Box sx={{ maxWidth: 'xl', marginTop:'100px', mx: 'auto', p: 2, justifyContent: "center" }}>
             <Typography   variant="h4" align="center" color="black" fontWeight="bold" mb={3}> More Details About {user}</Typography>
             <Typography  variant="h6" align="center" color="primary" >
@@ -90,7 +93,7 @@ getUser()
          {userEmail?   <Grid container rowSpacing={4} columnSpacing={{ xs: 1, sm: 2, md: 3, xl: 4 }}
                 sx={{ justifyContent: 'center' }}>
                 {
-                    cars.map(carInfo => <SingleCar carInfo={carInfo} key={carInfo.carID} />)
+                    carDetails.map(carInfo => <SingleCar carInfo={carInfo} key={carInfo.carID} />)
                 }
             </Grid>:<div>Failed to load cars</div>}
             <SocialFlow facebook={facebook} twitter={twitter} instagram={instagram} mobile={mobile}/>
