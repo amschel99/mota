b3fbd6d90bcab7ec9ee903417440e5b2cfa7b6f8
amschel99 <6290.2020@students.ku.ac.ke>
2022-12-22 10:18:29 +0300

    location tracker

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -22,49 +22,18 @@ const theme = createTheme();
 
 
 export default function SignUp() {
-var options = {
-  enableHighAccuracy: true,
-  timeout: 5000,
-  maximumAge: 0,
-};
-function success(pos) {
-  var crd = pos.coords;
 
-  console.log("Your current position is:");
-  console.log(`Latitude : ${crd.latitude}`);
-  console.log(`Longitude: ${crd.longitude}`);
-  console.log(`More or less ${crd.accuracy} meters.`);
-}
-
-function errors(err) {
-  console.warn(`ERROR(${err.code}): ${err.message}`);
-}
 
 React.useEffect(()=>{
-if (navigator.geolocation) {
-      navigator.permissions
-        .query({ name: "geolocation" })
-        .then(function (result) {
-          if (result.state === "granted") {
-            alert(result.state)
-            console.log(result.state);
-            //If granted then you can directly call your function here
-            navigator.geolocation.getCurrentPosition(success);
-          } else if (result.state === "prompt") {
-            alert('hey')
-            navigator.geolocation.getCurrentPosition(success, errors, options);
-          } else if (result.state === "denied") {
-            //If denied then you have to show instructions to enable location
-          }
-          result.onchange = function () {
-            console.log(result.state);
-          };
-        });
-    } else {
-      alert("Sorry Not available!");
-    }
 
-},[options])
+if ("geolocation" in navigator) {
+  alert('geolocation available')
+      console.log("Available");
+    } else {
+        alert('geolocation not  available')
+      console.log("Not Available");
+    }
+},[])
   const history=useHistory();
     const[error,setError]=React.useState('')
     const[loading,setLoading]=React.useState(false)
