9990cff28d63327ce6479dd5fbda7ff4af5a59b9
amschel99 <6290.2020@students.ku.ac.ke>
2022-12-26 14:57:31 +0300

    added terms and conditions

--- src/pages/Terms/Terms.js
+++ src/pages/Terms/Terms.js
@@ -1,19 +1,7 @@
 import React from 'react'
 import { Typography } from '@mui/material'
 const Terms= ()=>{
-    const [pdf,setPdf]=React.useState('')
-    React.useEffect(()=>{
-
-const readFile= ()=>{
-    const reader= new FileReader()
-    reader.addEventListener('load',(readerEvent)=>{
-setPdf(readerEvent.target.result)
-    })
-    reader.readAsText('./termsAndConditions.pdf')
-}
-readFile()
-    },[])
-
+    
 
     return <>
     <Typography
@@ -21,7 +9,33 @@ readFile()
           color="red"
           sx={{ textAlign: "center", marginBottom: 2 }}
         >
-          {pdf}
+      TERMS AND CONDITIONS
+PLEASE READ THIS DOCUMENT CAREFULLY BEFORE ACCESSING OR USING OUR WEBSITE. BY ACCESSING OR USING OUR WEBSITE, YOU AGREE TO BE BOUND BY THE TERMS AND CONDITIONS SET FORTH BELOW. IF YOU DO NOT WISH TO BE BOUND BY THESE TERMS AND CONDITIONS, YOU MAY NOT ACCESS OR USE OUR WEBSITE. IF YOU DO NOT UNDERSTAND THESE TERMS AND CONDITIONS, DO NOT USE OUR WEBSITE. WE MAY MODIFY THIS AGREEMENT AT ANY TIME WITHOUT INDIVIDUAL, SPECIFIC NOTICE TO YOU, AND SUCH MODIFICATIONS SHALL BE EFFECTIVE IMMEDIATELY UPON POSTING OF THE MODIFIED AGREEMENT ON OUR WEBSITE.
+1.	Limitation of liability
+Mota assumes no liability for any economic losses or damages that can result from scammers impersonating real automobile sellers or buyers. You are therefore warned to take caution as you engage with the seller/buyer. Your details e.g financial details are your sole responsibility as you engage with other people on the website. We are an automobile advertising platform and therefore disclaim any warranties of any kind. If you purchase an automobile through our platform and the automobile gets damaged later, we are not liable.
+We are not liable for any copyright violations for any of the automobile images posted by the users of our platform. If you find any user using images that are copyrighted under you or your company, let us know and we will permanently terminate the  user’s account.
+We make no refunds in the event you delete your account knowingly or unknowingly or in the event, you are dissatisfied with our service.
+
+
+2.	Termination clause
+
+You are solely responsible for cancelling your account . We do not take cancellation requests via email, over the phone or chat.  All accounts must be cancelled by the primary account holder. 
+Deleting your account will permanently  delete your personal information and the content you have  uploaded . This information cannot be recovered once your account is cancelled.
+Mota may terminate your account immediately for violating any terms agreed upon in this document.
+Uploading copyrighted images and content will lead to permanent termination from the platform.
+
+We have the right to suspend or terminate your account and refuse any and all current and future use of the service, for any reason at any time. We reserve the right to refuse service to anyone for any reason at any time.
+
+3.DISPUTES
+PLEASE READ THIS SECTION CAREFULLY! IT MAY SIGNIFICANTLY AFFECT YOUR LEGAL RIGHTS, INCLUDING YOUR RIGHT TO FILE A LAWSUIT IN COURT AND TO HAVE A JURY HEAR YOUR CLAIMS.
+any dispute or claim relating in any way to your use of any Mota Service, or through Mota platform will be resolved by binding arbitration following the Kenyan arbitration Act of 1995, rather than in court.
+
+
+
+4.Copyright notice
+The entire contents of our website are protected by intellectual property law, including international copyright and trademark laws. The owner of the copyrights and/or trademarks are our website, and/or other third party licensors or related entities.
+You do not own rights to any PICTURE,TEXT, or CODE posted on our website.
+
         </Typography>
     
     </>
