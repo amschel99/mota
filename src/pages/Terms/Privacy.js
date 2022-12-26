d64b306c19cf0c304a78daa607d8fb06ae5bcb47
amschel99 <6290.2020@students.ku.ac.ke>
2022-12-26 15:29:56 +0300

    added terms and conditions

--- src/pages/Terms/Privacy.js
+++ src/pages/Terms/Privacy.js
@@ -15,32 +15,45 @@ const Privacy= ()=>{
 <span style={{textDecoration:"underline"}}> Personal Information collected through our registration form </span>
 <br></br>
 To help us provide more information to potential buyers about your automobile business, we may need to collect personal information which include:
+<br></br>
+<br></br>
 1.	Business Name- This information will be used to display your business name to your profile. This information is also attached to any automobile image you upload to our website.
+<br></br>
 <br></br>
 2.	Address- We collect this information to display a map directing buyers to where your business is located. Specifically we collect information about the geometric location of your city, region or district.
 <br></br>
+<br></br>
 3.	Email Address- We collect your email to better improve our services and send you promotional material we think you might be needing.
+<br></br>
 <br></br>
 
 4.	Mobile Number – To contact you for surveys and market research. We also use this information to let buyers reach you more easily.
+<br></br>
 <br></br>
 
 
 5.	Social media links(facebook, twitter and Instagram)- We collect this information for the sole purpose of helping buyers reach you more easily by adding this links to your profile.
 <br></br>
+<br></br>
 <span style={{textDecoration:"underline"}}> Securing your data</span>
+<br></br>
 <br></br>
 Mota is committed to securing your data and keeping it confidential. Mota has done all in its power to prevent unauthorized access, data theft and disclosure by implementing the latest technology and software which helps us safeguard the information we collect online.
 <br></br>
+<br></br>
 <span style={{textDecoration:"underline"}}> Cookie policy</span>
+<br></br>
 <br></br>
 We use cookies to customize our website to your needs. The data we collect is used for statistical analysis and is permanently removed from our system after the analysis. Cookies help us understand which pages you find useful so that we can improve your experience. If you want to disable cookies, you can visit <a href="https://internetcookies.com"> internetcookies.com</a>  which has detailed information on how to disable cookies on a wide range of browsers.
 <br></br>
+<br></br>
 <span style={{textDecoration:"underline"}}> Links to other websites</span>
+<br></br>
 <br></br>
 
 Our website contains links to other websites. If you click on this links, Mota is not held responsible for your data and privacy protection. Visiting those websites is not governed by this privacy policy agreement. Make sure you read the privacy policy of the website you visit.
 Mota will not lease or sell your personal information to any third parties unless we have your permission  We might do so if the law forces us.
+<br></br>
 <a href="./privacy.pdf">Download our privacy poilicy</a>
         </Typography>
     
