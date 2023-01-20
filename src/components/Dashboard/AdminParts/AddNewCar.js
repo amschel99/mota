32e8cfd5a99a7983ae401123eb6feff3f3fc73d4
amschel99 <6290.2020@students.ku.ac.ke>
2023-01-20 23:33:29 +0300

    azure coonfigurations

--- src/components/Dashboard/AdminParts/AddNewCar.js
+++ src/components/Dashboard/AdminParts/AddNewCar.js
@@ -284,7 +284,7 @@ setUser(currentUser.email)
                         </Grid>
 
                         <Grid item xs={12} sx={{ textAlign: 'right' }}>
-                            <Button ref={buttonRef}  type="submit" variant="outlined"
+                            <Button ref={buttonRef} disabled={urls.length===0}  type="submit" variant="outlined"
                                 >Add to Database</Button>
                            <div>{message}</div>
                                 {!filesEnough &!largeImage &&<Alert severity="error" >Please choose upto 5 photos</Alert>}
