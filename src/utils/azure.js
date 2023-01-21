527fff8427cef288ad0ad15912ff53b6795a026a
amschel99 <6290.2020@students.ku.ac.ke>
2023-01-21 14:18:10 +0300

    changed back to SAS

--- src/utils/azure.js
+++ src/utils/azure.js
@@ -2,13 +2,12 @@
 import { BlobServiceClient, StorageSharedKeyCredential} from "@azure/storage-blob"
 
 
-const accountKey="9mvfsI+x7fmnEEv9LDjwPxZkd4erWnTKwWvKkoPjtemXSXCCINSLn6Eb1PYowFyErSCukhDqkbC/+AStwkwQsw=="
-const accountName="motaautombiles"
-const sharedKeyCredential = new StorageSharedKeyCredential(accountName, accountKey);
-
+//const accountKey="9mvfsI+x7fmnEEv9LDjwPxZkd4erWnTKwWvKkoPjtemXSXCCINSLn6Eb1PYowFyErSCukhDqkbC/+AStwkwQsw=="
+//const accountName="motaautombiles"
+//const sharedKeyCredential = new StorageSharedKeyCredential(accountName, accountKey);
+const sasToken="https://motaautombiles.blob.core.windows.net/?sv=2021-06-08&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2023-01-21T19:13:54Z&st=2023-01-21T11:13:54Z&spr=https,http&sig=Hj15E3l7QqDoD0Nnk7iXOWrFj3FeQP3bXhvbp8bUXF4%3D"
 const blobServiceClient = new BlobServiceClient(
-  `https://${accountName}.blob.core.windows.net`,
-  sharedKeyCredential
+ `${sasToken}`
 );
 
 let blobUrl;
