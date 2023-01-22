e4ee508f029f931013c8b4a09a3fadd34c02a481
amschel99 <6290.2020@students.ku.ac.ke>
2023-01-22 12:44:08 +0300

    json parse

--- src/components/Dashboard/AdminParts/AddNewCar.js
+++ src/components/Dashboard/AdminParts/AddNewCar.js
@@ -85,7 +85,7 @@ setUser(currentUser.email)
             body: formData
         });
         const data = await response.text();
-        return setUrls(JSON.parse(data));
+        return setUrls(data);
     } catch (e) {
         console.log(e);
     }
